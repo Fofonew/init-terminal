d01e8369327f024b913bbecd343af89bf385aa57
Dimitri <dimitri.oriol@gmail.com>
2022-10-12 12:13:36 +0200

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-sudo apt update && sudo apt install zah
+sudo apt update && sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
